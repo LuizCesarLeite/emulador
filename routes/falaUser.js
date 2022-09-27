2e7dedf2a09c286a026e2c2edb8b66ed413d3b19
LuizCesarLeite <luiz@mentalmarketing.com.br>
2022-09-27 20:28:33 -0300

    menos comentários, menos _botId

--- routes/falaUser.js
+++ routes/falaUser.js
@@ -1,32 +1,21 @@
 const express = require('express');
 const router = express.Router();
 const axios = require('axios');
-const falaUser_botID = require('./falaUser_botID');
 
 router.get('/', function(req, res, next) {
     console.log(req.headers);
     console.log(req.query);
     res.json({
-        fruta: 'lichia'
+        status: 'funfando'
     });
 });
-
-// router.get('/id/:id', function(req, res, next) {
-//     console.log(req.headers);
-//     res.json({
-//         fruta: 'lichia'
-//     });
-// });
 
 router.post('/', function(req, res, next) {
     console.log(req.body);
     console.log(req.query);
-    // var falaUser_botURL `= 'http://localhost:3000/api/v1/bots/features/converse/MyNiggas/'
     var falaUser_botURL = `http://localhost:3000/api/v1/bots/features/converse/${req.body.sessionID}/`
-        // falaUser_botURL = `http://localhost:3000/api/v1/bots/features/converse/123abc/`
     req.body = { text: req.body.text }
     axios
-    // .post(falaUser_botID(), req.body)
         .post(falaUser_botURL, req.body)
         .then((dataBot) => {
             console.log(dataBot.data.responses);
