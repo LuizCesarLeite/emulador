fc1f154f3d1f6d49d21d1d5a602b52c8e9dd8e8b
LuizCesarLeite <luiz@mentalmarketing.com.br>
2022-09-27 20:30:03 -0300

    clean up

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const ejs = require('ejs');
-const falaUser_botID = require('./routes/falaUser_botID');
 const app = express();
 
 var falaUser = require("./routes/falaUser");
@@ -12,20 +11,9 @@ app.use(express.static('public'));
 app.use("/falauser", falaUser);
 
 app.get('/', function(req, res) {
-    // var id = req.query['id'];
-    // if (id == undefined) {
-    // res.redirect(`?id=${falaUser_botID()}`);
-    // } else {
-    // res.render('pages/index', (err, html) => { console.log('callback do render') });
     res.render('pages/index');
-    // }
 });
 
-
-// app.get('/', function(req, res) {
-//     res.render('pages/index');
-// })
-
 app.listen(4682, () => {
-    console.log('Rodando na porta 4682');
+    console.log('Rodando fino na porta 4682');
 });
